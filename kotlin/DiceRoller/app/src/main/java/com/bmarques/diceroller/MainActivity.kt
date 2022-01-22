a97785efb5591ed48b9af1b5abe5aa87c85e5866
Tiago Marques <tiagobm564@gmail.com>
2022-01-22 22:45:11 +0400

    Added more project

--- kotlin/DiceRoller/app/src/main/java/com/bmarques/diceroller/MainActivity.kt
+++ kotlin/DiceRoller/app/src/main/java/com/bmarques/diceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.bmarques.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -9,40 +10,55 @@ import java.lang.Integer.parseInt
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        diceImage = findViewById(R.id.dice_image)
+
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
-        val countUpButton: Button = findViewById(R.id.countup_button)
-        countUpButton.setOnClickListener { countUp() }
 
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = "Dice Rolled!"
+//        val countUpButton: Button = findViewById(R.id.countup_button)
+//        countUpButton.setOnClickListener { countUp() }
+
+//        val resultText: TextView = findViewById(R.id.result_text)
+//        resultText.text = "Dice Rolled!"
 
     }
 
-    private fun countUp() {
-        val resultText: TextView = findViewById(R.id.result_text)
-
-        val numeric = resultText.text.matches("-?\\d+(\\.\\d+)?".toRegex())
-
-        if (!numeric) return
-
-        val textInt = parseInt(resultText.text.toString())
-
-        if (textInt >= 6) return
-
-        resultText.text = (textInt + 1).toString()
-    }
+//    private fun countUp() {
+//        val resultText: TextView = findViewById(R.id.result_text)
+//
+//        val numeric = resultText.text.matches("-?\\d+(\\.\\d+)?".toRegex())
+//
+//        if (!numeric) return
+//
+//        val textInt = parseInt(resultText.text.toString())
+//
+//        if (textInt >= 6) return
+//
+//        resultText.text = (textInt + 1).toString()
+//    }
 
     private fun rollDice() {
 //        Toast.makeText(this, "button clicked",
 //            Toast.LENGTH_SHORT).show()
-        val randomInt = (1..6).random()
 
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+        val drawableResource = when ((1..6).random()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+
+//        val resultText: TextView = findViewById(R.id.result_text)
+//        resultText.text = randomInt.toString()
     }
 }
